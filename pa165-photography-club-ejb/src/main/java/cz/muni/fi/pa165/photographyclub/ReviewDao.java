5d981f5aa0b0b71af7f7cc98a6fbc129f1888c4c
melanger <melanger@users.noreply.github.com>
2017-10-28 11:27:42 +0200

    created ReviewDao interface

---
+++ pa165-photography-club-ejb/src/main/java/cz/muni/fi/pa165/photographyclub/ReviewDao.java
@@ -0,0 +1,42 @@
+package cz.muni.fi.pa165.photographyclub;
+
+import java.util.List;
+
+/**
+ * @author Pavel Brousek
+ */
+public interface ReviewDao {
+    /**
+     * Retrieve a review based on id.
+     * @param id review id
+     * @return review if found, null otherwise
+     */
+    public Review findById(Long id);
+    /**
+     * Create (store) a new review.
+     * @param r review to be stored
+     */
+    public void create(Review r);
+    /**
+     * Remove (delete) a review.
+     * @param r review to be deleted
+     */
+    public void remove(Review r);
+    /**
+     * Get all reviews.
+     * @return list of reviews (might be empty)
+     */
+    public List<Review> findAll();
+    /**
+     * Get reviews of a member.
+     * @param m member whose reviews should be fetched
+     * @return list of reviews of the member (might be empty)
+     */
+    public List<Review> findByAuthor(Member m);
+    /**
+     * Get reviews of a tour.
+     * @param t tour for which reviews should be fetched
+     * @return list of reviews for the tour (might be empty)
+     */
+    public List<Review> findByTour(Tour t);
+}
