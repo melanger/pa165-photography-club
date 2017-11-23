025d3a6cb2008efba02869be659eebe92f257dc9
Denis.Figula <denis.figula@embedit.cz>
2017-11-23 22:17:52 +0100

    ReviewService, ReviewServiceImpl added. Tests will be added later
    closes #54

--- pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/EquipmentService.java
+++ pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/EquipmentService.java
@@ -1,8 +1,21 @@
 package cz.muni.fi.pa165.photographyclub.service;
 
 import cz.muni.fi.pa165.photographyclub.entity.Equipment;
+import cz.muni.fi.pa165.photographyclub.entity.Member;
+import cz.muni.fi.pa165.photographyclub.enums.EquipmentType;
+
+import java.util.List;
+import java.util.Map;
 
 public interface EquipmentService extends GenericService<Equipment> {
+
+    List<Equipment> findEquipmentByOwner(Member m);
+
+    /**
+     * Method gets all equipment and sort it by type
+     * @return map containing EquipmentType key and List of associated equipment
+     */
+    Map<EquipmentType,List<Equipment>> getEquipmentInventoryByType();
 
 
 }
--- pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/EquipmentServiceImpl.java
+++ pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/EquipmentServiceImpl.java
@@ -2,9 +2,14 @@ package cz.muni.fi.pa165.photographyclub.service;
 
 import cz.muni.fi.pa165.photographyclub.dao.EquipmentDao;
 import cz.muni.fi.pa165.photographyclub.entity.Equipment;
+import cz.muni.fi.pa165.photographyclub.entity.Member;
+import cz.muni.fi.pa165.photographyclub.enums.EquipmentType;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EquipmentServiceImpl implements EquipmentService {
 
@@ -29,5 +34,23 @@ public class EquipmentServiceImpl implements EquipmentService {
     @Override
     public void remove(Equipment e) {
         equipmentDao.remove(e);
+    }
+
+    @Override
+    public List<Equipment> findEquipmentByOwner(Member m) {
+        return equipmentDao.findByOwner(m);
+    }
+
+    @Override
+    public Map<EquipmentType, List<Equipment>> getEquipmentInventoryByType() {
+        Map<EquipmentType, List<Equipment>> inventory = new HashMap<>();
+
+        for (EquipmentType t : EquipmentType.values()) {
+            List<Equipment> equipmentList = new ArrayList<>();
+            for (Equipment e : equipmentDao.findAll())
+                if (e.getType().equals(t)) equipmentList.add(e);
+            inventory.put(t,equipmentList);
+        }
+        return inventory;
     }
 }
