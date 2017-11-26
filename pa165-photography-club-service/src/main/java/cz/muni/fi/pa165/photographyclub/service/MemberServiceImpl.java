e84c18eb76354145c08452119010339abc3bc670
matus <matus@147.251.201.43>
2017-11-26 17:25:54 +0100

    MemberService, MemberServiceImpl, MemberServiceTest

---
+++ pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/MemberServiceImpl.java
@@ -0,0 +1,27 @@
+package cz.muni.fi.pa165.photographyclub.service;
+
+import cz.muni.fi.pa165.photographyclub.dao.GenericDao;
+import cz.muni.fi.pa165.photographyclub.dao.MemberDao;
+import cz.muni.fi.pa165.photographyclub.entity.Member;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author matus
+ */
+public class MemberServiceImpl extends GenericServiceImpl<Member> implements MemberService {
+
+    @Inject
+    protected MemberDao dao;
+    
+    @Override
+    protected GenericDao<Member> getDao() {
+        return dao;
+    }
+
+    @Override
+    public Member findByName(String name) {
+        return dao.findByName(name);
+    }
+    
+}
