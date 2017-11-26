a87fb8c86774963aa742717b89e9f89dd44a8dd3
melanger <melanger@users.noreply.github.com>
2017-11-26 10:26:31 +0100

    TourService

---
+++ pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/TourService.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pa165.photographyclub.service;
+
+import cz.muni.fi.pa165.photographyclub.entity.Tour;
+
+/**
+ * Service for manipulating Tours.
+ * @author Pavel Brousek
+ */
+public interface TourService extends GenericService<Tour> {
+    
+}
