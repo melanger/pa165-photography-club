526c7e8066815a209dfb33de6f6261760934a623
melanger <melanger@users.noreply.github.com>
2017-12-12 16:44:01 +0100

    javadoc for BeanMappingService (closes #92)

--- pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/beanmapping/BeanMappingService.java
+++ pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/beanmapping/BeanMappingService.java
@@ -7,9 +7,27 @@ import org.dozer.Mapper;
 
 
 public interface BeanMappingService {
-	
+    /**
+     * Map a collection of objects to a list of objects of the mapToClass.
+     * @param <T> the target type
+     * @param objects collection of source objects
+     * @param mapToClass target class
+     * @return list of mapped objects
+     */
     public  <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass);
-
+    
+    /**
+     * Map an object to an object of the mapToClass.
+     * @param <T> the target type
+     * @param u source object
+     * @param mapToClass target class
+     * @return the mapped object
+     */
     public  <T> T mapTo(Object u, Class<T> mapToClass);
+    
+    /**
+     * Getter for mapper.
+     * @return Mapper
+     */
     public Mapper getMapper();
 }
