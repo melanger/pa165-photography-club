29e337c74afeceb011b7ea094f61564c202c96d5
melanger <melanger@users.noreply.github.com>
2017-12-12 16:49:11 +0100

    javadoc for GenericService (closes #93)

--- pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/GenericService.java
+++ pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/GenericService.java
@@ -3,11 +3,32 @@ package cz.muni.fi.pa165.photographyclub.service;
 import java.util.List;
 
 /**
+ * A generic service.
  * @author Pavel Brousek
  */
 public interface GenericService<T> {
+    /**
+     * Create (save) an entity instance.
+     * @param e entity instance
+     */
     void create(T e);
+    
+    /**
+     * Find an entity instance by id.
+     * @param id id of the entity instance
+     * @return the entity instance if found, null otherwise
+     */
     T findById(Long id);
+    
+    /**
+     * Get a list of all instances of the entity.
+     * @return List of entity instances (might be empty)
+     */
     List<T> findAll();
+    
+    /**
+     * Remove an entity instance.
+     * @param e entity instance to be removed
+     */
     void remove(T e);
 }
