b621f9563cc2ad4c83412ef6afe6e86888355d85
melanger <melanger@users.noreply.github.com>
2017-12-12 15:58:32 +0100

    ServiceApplicationContext (for users of service module)

--- pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/config/ServiceApplicationContext.java
+++ pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/service/config/ServiceApplicationContext.java
@@ -1,14 +1,18 @@
 package cz.muni.fi.pa165.photographyclub.service.config;
 
+import cz.muni.fi.pa165.photographyclub.PersistenceSampleApplicationContext;
+import cz.muni.fi.pa165.photographyclub.beanmapping.config.BeanMappingApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 
 /**
  * @author Pavel Brousek
  */
 @Configuration
-@ComponentScan(basePackages = {"cz.muni.fi.pa165.photographyclub.service.dummy"})
+@Import({PersistenceSampleApplicationContext.class, BeanMappingApplicationContext.class})
+@ComponentScan(basePackages = {"cz.muni.fi.pa165.photographyclub.service", "cz.muni.fi.pa165.photographyclub.facade"})
 public class ServiceApplicationContext {
-
+    
 }
