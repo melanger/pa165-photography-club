0eb754ef45d0c6462475adc18136b3bc72e4d2c0
Denis.Figula <denis.figula@embedit.cz>
2017-12-13 18:22:50 +0100

    Finished methods from equipment facade in equipmentController closes #115

--- pa165-photography-club-rest/src/main/java/cz/muni/fi/pa165/photographyclub/rest/controller/EquipmentController.java
+++ pa165-photography-club-rest/src/main/java/cz/muni/fi/pa165/photographyclub/rest/controller/EquipmentController.java
@@ -1,15 +1,16 @@
 package cz.muni.fi.pa165.photographyclub.rest.controller;
 
+import cz.muni.fi.pa165.photographyclub.dto.EquipmentCreateDTO;
 import cz.muni.fi.pa165.photographyclub.dto.EquipmentDTO;
 import cz.muni.fi.pa165.photographyclub.facade.EquipmentFacade;
 import cz.muni.fi.pa165.photographyclub.rest.ApiUris;
 import cz.muni.fi.pa165.photographyclub.rest.exception.ResourceNotFoundException;
 import javax.inject.Inject;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
+
+import java.util.List;
 
 /**
  * REST Controller for Equipment
@@ -38,4 +39,43 @@ public class EquipmentController {
 
         return equipmentDTO;
     }
+
+    /**
+     * Get list of equipment associated to member
+     *
+     * @param memberId id of member
+     * @return list of equipment
+     * @throws Exception ResourceNotFoundException if there is empty result
+     */
+    @RequestMapping(value = "by_member_id/{member_id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final List<EquipmentDTO> getEquipmentByMember(@PathVariable("member_id") long memberId) throws Exception{
+        List<EquipmentDTO> equipmentDTOList = equipmentFacade.getEquipmentByMember(memberId);
+        if (equipmentDTOList == null) {
+            throw new ResourceNotFoundException();
+        }
+        return equipmentDTOList;
+    }
+
+    /**
+     * Adds equipment to member
+     *
+     * @param memberId id of member
+     * @param equipmentCreateDTO equipment to be added to member
+     */
+    @RequestMapping(value = "/{member_id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final void addEquipmentToMember(@PathVariable("member_id") long memberId, @RequestBody EquipmentCreateDTO equipmentCreateDTO){
+        equipmentFacade.addEquipmentToMember(memberId,equipmentCreateDTO);
+    }
+
+    /**
+     * Remove equipment from member
+     *
+     * @param memberId id of member
+     * @param equipmentId equipment to be removed from member
+     */
+    @RequestMapping(value = "/{member_id},{equipment_id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final void removeEquipmentFromMember(@PathVariable("member_id") long memberId, @PathVariable("equipment_id") long equipmentId){
+        equipmentFacade.removeEquipmentOfMember(memberId,equipmentId);
+    }
+
 }
