c523effdc517f5564081063b5cd4c5bde6a9835b
melanger <melanger@users.noreply.github.com>
2017-12-15 20:58:44 +0100

    removed unused imports

--- pa165-photography-club-data/src/main/java/cz/muni/fi/pa165/photographyclub/sample/SampleDataLoadingFacadeImpl.java
+++ pa165-photography-club-data/src/main/java/cz/muni/fi/pa165/photographyclub/sample/SampleDataLoadingFacadeImpl.java
@@ -18,11 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
