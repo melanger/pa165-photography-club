2c1730f461ffda484b436a104d65f1cf885a938b
Denis.Figula <denis.figula@embedit.cz>
2017-12-17 22:33:00 +0100

    getEquipmentByMember method adjusted

--- pa165-photography-club-rest/src/main/java/cz/muni/fi/pa165/photographyclub/rest/controller/EquipmentController.java
+++ pa165-photography-club-rest/src/main/java/cz/muni/fi/pa165/photographyclub/rest/controller/EquipmentController.java
@@ -50,16 +50,13 @@ public class EquipmentController {
      * @return list of equipment
      * @throws Exception ResourceNotFoundException if there is empty result
      */
-    @RequestMapping(value = "members/{member_id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{member_id}/equipment", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public final List<EquipmentDTO> getEquipmentByMember(@PathVariable("member_id") long memberId) throws Exception{
-//        try{
-//            return equipmentFacade.getEquipmentByMember(memberId);
-//        } catch (Exception e){
-//            throw new ResourceNotFoundException();
-//        }
-        List<EquipmentDTO> equipmentDTOList = equipmentFacade.getEquipmentByMember(memberId);
-        if (equipmentDTOList == null) throw new ResourceNotFoundException();
-        else return equipmentDTOList;
+        try{
+            return equipmentFacade.getEquipmentByMember(memberId);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException();
+        }
     }
 
     /**
