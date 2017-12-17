056306277db0f60b05f9cecb5ede072cb88a15aa
melanger <melanger@users.noreply.github.com>
2017-12-18 00:59:12 +0100

    renamed (Photo)EntityNotFoundException

--- pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/facade/PhotoEntityNotFoundException.java
+++ pa165-photography-club-service/src/main/java/cz/muni/fi/pa165/photographyclub/facade/PhotoEntityNotFoundException.java
@@ -1,15 +1,16 @@
 package cz.muni.fi.pa165.photographyclub.facade;
 
 import cz.muni.fi.pa165.photographyclub.entity.PhotoEntity;
+import javax.persistence.EntityNotFoundException;
 
-public class EntityNotFoundException extends IllegalArgumentException {
+public class PhotoEntityNotFoundException extends EntityNotFoundException {
     Class entityClass;
     
     public <T extends PhotoEntity> Class<T> getEntityClass() {
         return entityClass;
     }
     
-    public <T extends PhotoEntity> EntityNotFoundException(Class<T> entityClass) {
+    public <T extends PhotoEntity> PhotoEntityNotFoundException(Class<T> entityClass) {
         super(entityClass.getSimpleName() + " entity not found");
         this.entityClass = entityClass;
     }
